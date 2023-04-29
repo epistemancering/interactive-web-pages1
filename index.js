bf194c2028c61d5f6d83aad0896b230b2acc32d4
Austin Henrie <nitsua15@gmail.com>
2023-04-28 19:30:15 -0600

    completed intermediate

--- index.js
+++ index.js
@@ -17,14 +17,22 @@ function addMovie(event) {
 }
 function deleteMovie(event) {
     event.target.parentNode.remove()
-    message.textContent = 'Movie deleted!'
+    message.textContent = event.target.parentNode.children[0].textContent + ' deleted!'
+    revealMessage()
 }
 function crossOffMovie(event) {
     event.target.classList.toggle('checked')
     if (event.target.classList.contains('checked')) {
-        message.textContent = 'Movie watched!'
+        message.textContent = event.target.textContent + ' watched!'
     } else {
-        message.textContent = 'Movie added back!'
+        message.textContent = event.target.textContent + ' added back!'
     }
+    revealMessage()
+}
+function revealMessage() {
+    message.classList.remove('hide')
+    setTimeout(function() {
+        message.classList.add('hide')
+    }, 1000)
 }
 document.querySelector('form').addEventListener('submit', addMovie)
